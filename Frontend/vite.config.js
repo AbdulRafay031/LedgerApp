11c2a5e22e114dc02bc88027962b8f57f4e6335e
ABDUL RAFAY <rafay16031@gmail.com>
2025-02-09 21:44:22 +0500

    vite error resolved

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'index.html', // Ensure Vite knows the location of the HTML entry point
-    },
-  },
 })
-
